77a5c885b5dd8decc64937f7e0a8bd3e1258a178
Sachin Makaram <smakaram@smakaram-m01.vmware.com>
2017-08-18 11:59:24 -0700

    smtp

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$comment = $_POST["comment"];
-
-	$to = "maksach@gmail.com";
-	$subject = "Comment from " . $name;
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$comment = $_POST['comment'];
+	$to = 'maksach@gmail.com';
+	$subject = 'Comment from ' . $name;
 	$text = $comment . "\n\n" . $email;
-	$headers = "From: " . $email . "\r\n" . 
-				"CC: maksach@gmail.com";
+	$headers = 'From: ' . $email . "\r\n" . 
+				'CC: maksach@gmail.com';
 	// . = concatenation \n = new line filezilla: uploading to web hosting service
 	 //UW student server = vergil
-	mail ($to, $subject, $text, $headers);
+	mail($to, $subject, $text, $headers);
 
 	header("Location: index.html");
 	die();
